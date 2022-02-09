62e30016a9b0b8c032b7d3d77f6e4e496957e821
Rina-01 <ira.mashkova.2001@mail.ru>
2022-02-09 17:48:16 +0300

    Update main.js

---
+++ main.js
@@ -0,0 +1,23 @@
+const player1 = {
+    name: 'Saber',
+    hp: 100,
+    img: '',
+    weapon: ['Excalibur', 'Arondight', 'Clarent', 'Crocea Mors', 'Beagalltach', 'Moralltach'], 
+    attack: function() {
+        console.log(this.name + ' Fight...');
+    }
+};
+
+const player2 = {
+    name: 'Lancer',
+    hp: 100,
+    img: '',
+    weapon: ['Gae Bolg', 'Gei Bou', 'Gei Jarugu', 'Rhongomyniad', 'Gungner', 'Aeglos'],
+    attack: function() {
+        console.log(this.name + ' Fight...');
+    }
+};
+
+function createPlayer() {
+
+}
