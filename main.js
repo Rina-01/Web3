fcddc791ab1ad78c5c0c685ae030d3db0455ba55
Rina-01 <ira.mashkova.2001@mail.ru>
2022-02-09 20:22:02 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const player1 = {
     name: 'Saber',
-    hp: 100,
-    img: '',
+    hp: 90,
+    img: 'http://reactmarathon-api.herokuapp.com/assets/sonya.gif',
     weapon: ['Excalibur', 'Arondight', 'Clarent', 'Crocea Mors', 'Beagalltach', 'Moralltach'], 
     attack: function() {
         console.log(this.name + ' Fight...');
@@ -10,14 +10,48 @@ const player1 = {
 
 const player2 = {
     name: 'Lancer',
-    hp: 100,
-    img: '',
+    hp: 80,
+    img: 'http://reactmarathon-api.herokuapp.com/assets/subzero.gif',
     weapon: ['Gae Bolg', 'Gei Bou', 'Gei Jarugu', 'Rhongomyniad', 'Gungner', 'Aeglos'],
     attack: function() {
         console.log(this.name + ' Fight...');
     }
 };
 
-function createPlayer() {
+function createPlayer(cls, obcls) {
+    const $player = document.createElement('div');
+    $player.className = cls;
+    
 
+    const $progres = document.createElement('div');
+    $progres.className = 'progressbar';
+
+    const $life = document.createElement('div');
+    $life.className = 'life';
+    $life.innerText = obcls.hp;
+    $progres.appendChild($life);
+    const $name = document.createElement('div');
+    $name.className = 'name';
+    $name.innerText = obcls.name;
+    $progres.appendChild($name);
+
+    $player.appendChild($progres);
+
+
+    const $char = document.createElement('div');
+    $char.className = 'character';
+
+    const $img = document.createElement('img');
+    $img.src = obcls.img;
+    $char.appendChild($img);
+   
+    $player.appendChild($char);
+
+
+    const $arena = document.getElementsByClassName('arenas');
+    $arena.appendChild($player)
 }
+
+
+createPlayer('player1', player1);
+createPlayer('player2', player2);
