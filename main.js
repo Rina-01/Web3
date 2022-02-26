96a72c4a07864ef9013390d04ed74943d382c3a4
Rina-01 <ira.mashkova.2001@mail.ru>
2022-02-26 22:07:38 +0300

    homework-5

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
 const $arena = document.querySelector('.arenas');
-const $randomButton = document.querySelector('.button');
+// const $randomButton = document.querySelector('.button');
+const $formFight = document.querySelector('.conrol');
+
+const HIT = {
+    head: 30,
+    body: 25,
+    foot: 20,
+}
+const ATTACK = ['head', 'body', 'foot'];
 
 const player1 = {
     player: 1,
@@ -24,6 +32,26 @@ const player2 = {
     elHP,
     renderHP
 };
+
+function attack() {
+    console.log(this.name + ' Fight...');
+}
+
+function changeHP(damage) {
+    this.hp -= damage;
+    if (this.hp < 0) {
+        this.hp = 0;
+    } 
+}
+
+function elHP() {
+    return document.querySelector('.player' + this.player + ' .life');
+}
+
+function renderHP() {
+    this.elHP().style.width = this.hp + '%';
+}
+
 
 function createElement(tag, classNam) {
     const $tag = document.createElement(tag);
@@ -61,23 +89,9 @@ function createPlayer(obcls) {
 $arena.appendChild(createPlayer(player1));
 $arena.appendChild(createPlayer(player2));
 
-function attack() {
-    console.log(this.name + ' Fight...');
-}
 
-function changeHP(damage) {
-    this.hp -= damage;
-    if (this.hp < 0) {
-        this.hp = 0;
-    } 
-}
-
-function elHP() {
-    return document.querySelector('.player' + this.player + ' .life');
-}
-
-function renderHP() {
-    this.elHP().style.width = this.hp + '%';
+function getRandom(num) {   // [ 0 ; num-1 ]
+    return Math.floor(Math.random()*num);    
 }
 
 function gameEnd(name) {
@@ -99,10 +113,38 @@ function createReloadButton() {
     return $reloadButton;
 }
 
+function enemyAttack() {
+    const hit = ATTACK[getRandom(3)];
+    const defence = ATTACK[getRandom(3)];
+    return {
+        value: getRandom(HIT[hit]),
+        hit,
+        defence
+    }
+}
+
+$formFight.addEventListener('submit', function(e) {
+    e.preventDefault();
+    enemyAttack();
+    const enemy = enemyAttack();    // player2
+    const me = {};
+    for (let i of $formFight) {
+        if (i.checked && i.name === 'hit') {
+            me.value = getRandom(HIT[i.value]);
+            me.hit = i.value;
+        }
+        if (i.checked && i.name === 'defence') {
+            me.defence = i.value;
+        }
+        i.checked = false;
+    }
+})
+
+/*
 $randomButton.addEventListener('click', function(){
-    player1.changeHP(1 + Math.ceil(Math.random()*20));
+    player1.changeHP(1 + getRandom(20));
     player1.renderHP();
-    player2.changeHP(1 + Math.ceil(Math.random()*20));
+    player2.changeHP(1 + getRandom(20));
     player2.renderHP();
 
     if (player1.hp==0 && player2.hp==0) {
@@ -120,5 +162,5 @@ $randomButton.addEventListener('click', function(){
             window.location.reload();
         });
     }
-});
-
+})
+*/
