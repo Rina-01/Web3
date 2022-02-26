9885015763fd917a4be8aef000acb1effc44984c
Rina-01 <ira.mashkova.2001@mail.ru>
2022-02-26 23:06:09 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,11 @@
 const $arena = document.querySelector('.arenas');
-// const $randomButton = document.querySelector('.button');
-const $formFight = document.querySelector('.conrol');
+const $formFight = document.querySelector('.control');
 
 const HIT = {
     head: 30,
     body: 25,
     foot: 20,
-}
+};
 const ATTACK = ['head', 'body', 'foot'];
 
 const player1 = {
@@ -15,7 +14,6 @@ const player1 = {
     hp: 100,
     img: 'http://reactmarathon-api.herokuapp.com/assets/sonya.gif',
     weapon: ['Excalibur', 'Arondight', 'Clarent', 'Crocea Mors', 'Beagalltach', 'Moralltach'], 
-    attack,
     changeHP,
     elHP,
     renderHP
@@ -27,15 +25,10 @@ const player2 = {
     hp: 100,
     img: 'http://reactmarathon-api.herokuapp.com/assets/subzero.gif',
     weapon: ['Gae Bolg', 'Gei Bou', 'Gei Jarugu', 'Rhongomyniad', 'Gungner', 'Aeglos'],
-    attack,
     changeHP,
     elHP,
     renderHP
 };
-
-function attack() {
-    console.log(this.name + ' Fight...');
-}
 
 function changeHP(damage) {
     this.hp -= damage;
@@ -65,22 +58,18 @@ function createPlayer(obcls) {
     const $player = createElement('div', 'player' + obcls.player);
 
     const $progres = createElement('div', 'progressbar');
-
     const $life = createElement('div', 'life');
     $life.style.width = $player.hp + '%';
     $progres.appendChild($life);
     const $name = createElement('div', 'name');
     $name.innerText = obcls.name;
     $progres.appendChild($name);
-
     $player.appendChild($progres);
 
     const $char = createElement('div', 'character');
-
     const $img = createElement('img');
     $img.src = obcls.img;
     $char.appendChild($img);
-   
     $player.appendChild($char);
 
     return $player;
@@ -94,25 +83,6 @@ function getRandom(num) {   // [ 0 ; num-1 ]
     return Math.floor(Math.random()*num);    
 }
 
-function gameEnd(name) {
-    const $loseTitle = createElement('div', 'loseTitle');
-    if (name) {
-        $loseTitle.innerText = name + ' wins';
-    } else {
-        $loseTitle.innerText = 'draw';
-    }
-    return $loseTitle;
-}
-
-function createReloadButton() {
-    const $reloadWrap = createElement('div', 'reloadWrap');
-    const $reloadButton = createElement('button', 'button');
-    $reloadButton.innerText = 'Restart';
-    $reloadWrap.appendChild($reloadButton);
-    $arena.appendChild($reloadWrap);
-    return $reloadButton;
-}
-
 function enemyAttack() {
     const hit = ATTACK[getRandom(3)];
     const defence = ATTACK[getRandom(3)];
@@ -123,29 +93,54 @@ function enemyAttack() {
     }
 }
 
+function playerAttack(formFight) {
+    const me = {
+        value: 0,
+        hit: '',
+        defence: ''
+    };
+    if (formFight) {
+        for (let i of formFight) {
+            if (i.checked && i.name === 'hit') {
+                me.value = getRandom(HIT[i.value]);
+                me.hit = i.value;
+            }
+            if (i.checked && i.name === 'defence') {
+                me.defence = i.value;
+            }
+            i.checked = false;
+        }
+    }
+    return me;
+}
+
+function fight() {
+    const me = playerAttack($formFight);    // player1
+    const enemy = enemyAttack();    // player2
+    if (me.hit != enemy.defence) {
+        player2.changeHP(me.value);
+        player2.renderHP();
+    }
+    if (enemy.hit != me.defence) {
+        player1.changeHP(enemy.value);
+        player1.renderHP();
+    }
+}
+
+function gameEnd(name) {
+    const $loseTitle = createElement('div', 'loseTitle');
+    if (name) {
+        $loseTitle.innerText = name + ' wins';
+    } else {
+        $loseTitle.innerText = 'draw';
+    }
+    return $loseTitle;
+}
+
 $formFight.addEventListener('submit', function(e) {
     e.preventDefault();
-    enemyAttack();
-    const enemy = enemyAttack();    // player2
-    const me = {};
-    for (let i of $formFight) {
-        if (i.checked && i.name === 'hit') {
-            me.value = getRandom(HIT[i.value]);
-            me.hit = i.value;
-        }
-        if (i.checked && i.name === 'defence') {
-            me.defence = i.value;
-        }
-        i.checked = false;
-    }
-})
 
-/*
-$randomButton.addEventListener('click', function(){
-    player1.changeHP(1 + getRandom(20));
-    player1.renderHP();
-    player2.changeHP(1 + getRandom(20));
-    player2.renderHP();
+    fight();
 
     if (player1.hp==0 && player2.hp==0) {
         $arena.appendChild(gameEnd());
@@ -154,13 +149,25 @@ $randomButton.addEventListener('click', function(){
     } else if (player2.hp==0 && player1.hp!=0) {
         $arena.appendChild(gameEnd(player1.name));
     }
+
     if (player1.hp==0 || player2.hp==0) {
-        $randomButton.disabled = true;
+        for (let i of $formFight) {
+            i.disabled = true;
+        }
 
         const $reloadButton = createReloadButton();
         $reloadButton.addEventListener('click', function() {
             window.location.reload();
         });
     }
-})
-*/
+});
+
+function createReloadButton() {
+    const $reloadWrap = createElement('div', 'reloadWrap');
+    const $reloadButton = createElement('button', 'button');
+    $reloadButton.innerText = 'Restart';
+    $reloadWrap.appendChild($reloadButton);
+    $arena.appendChild($reloadWrap);
+    return $reloadButton;
+}
+
